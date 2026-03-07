6eb4d38815f50ffb1f279590dc56968fab99d464
Gustavo Milhomens <gustavomilhomens.dev@gmail.com>
2026-03-06 22:27:22 -0300

    update rpg.py

--- py_dev/rpg.py
+++ py_dev/rpg.py
@@ -1,9 +1,15 @@
 class Jogador():
-    def __init__(self, nome, raca, classe, ):
-        self.nome = nome
-        self.raca = raca
-        self.classe = classe
-        self.arma = ''
+def __init__(self, nome, raca, classe,):
+self.nome = nome
+self.raca = raca
+self.classe = classe
+self.arma = ''
 
 jogador = Jogador(input('informe o nome do jogador: '), input('informe a raça: '), input('informe a classe: '))
-print(f"nome: {jogador.nome}\nraça: {jogador.raca}\nclasse: {jogador.classe}")
+print(f"nome: {
+    jogador.nome
+}\nraça: {
+    jogador.raca
+}\nclasse: {
+    jogador.classe
+}")
