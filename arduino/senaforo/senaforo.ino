ff197f04eb4f9326c1a9a86c392541c0c6bc32a9
Gustavo Milhomens <gustavo.santos23@estudante.ifto.edu.br>
2026-08-09 12:20:02 -0300

    arduino

---
+++ arduino/senaforo/senaforo.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+
+int led_verm = 2;
+int led_am = 3;
+int led_verd = 4;
+
+void setup()
+{
+  pinMode(led_verm, OUTPUT);
+  pinMode(led_am, OUTPUT);
+  pinMode(led_verd, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led_verd, HIGH);
+  delay(1000); // Wait for 10 millisecond(s)
+  digitalWrite(led_verd, LOW);
+  delay(1000);
+  
+  
+  digitalWrite(led_am, HIGH);
+  delay(1000); // Wait for 10 millisecond(s)
+  digitalWrite(led_am, LOW);
+  
+  digitalWrite(led_verm, HIGH);
+  delay(1000); // Wait for 10 millisecond(s)
+  digitalWrite(led_verm, LOW);
+}
---
+++ arduino/sensor_teste/sensor_teste.ino
@@ -0,0 +1,91 @@
+// C++ code// C++
+//
+int led_verde = 2; // pino do led verde
+int led_amarel = 3; // pino do led amarelo
+int led_verm = 4; // pino do led vermelho
+int buzer_pin = 5; // pino do buzzer
+int echo_pin = 6; // pino do echo do sensor
+int trig_pin = 7; // pino do trig do sensor
+
+
+int limite_distanc = 0; // limite de distancia
+int cm = 0; // centimetros
+
+long ler_distancia_sensor(int t_pin, int e_pin)
+{
+    pinMode(t_pin, OUTPUT); // determina o pino do trig/disparo do sensor como saida/output 
+    digitalWrite(t_pin, LOW); // deixa o pino em nivel baixo/desligado
+    delayMicroseconds(2); // espera de 2 microsegundos
+
+    digitalWrite(t_pin, HIGH); // faz o sensor emitir som colocando em nivel alto/ligado
+    delayMicroseconds(10); 
+    digitalWrite(t_pin, LOW);
+    pinMode(e_pin, INPUT); // configura o pino echo como entrada entrada/input
+
+    return pulseIn(e_pin, HIGH); // retorna ficar em hing e retorna o tempo levado em micrisegundos 
+
+}
+
+void setup()
+{
+    Serial.begin(9600); // inicia a cominicação em 9600 bits/segundo
+    pinMode(led_verde, OUTPUT); // configura o pino verde como saida 
+    pinMode(led_amarel, OUTPUT); // configura o pino amarelo como saida
+    pinMode(led_verm, OUTPUT); // configura o pino vermelho como saida
+    pinMode(buzer_pin, OUTPUT); // configura o buzzer como saida  
+}
+
+void loop()
+{
+    limite_distanc = 50; // limite de destancia de 350cm
+    cm = 0.01723 * ler_distancia_sensor( trig_pin , echo_pin); // converte o valor em centimetros com base nos microsegundos 
+    Serial.print("cm: ");
+    Serial.print(cm);
+    Serial.print("");
+
+
+
+    if (cm > limite_distanc) // se a distancia em cm for maior que o limite não manda nenhum sinal aos leds 
+    {
+        digitalWrite(led_verde, LOW);
+        digitalWrite(led_amarel, LOW);
+        digitalWrite(led_verm, LOW);      
+
+    }
+
+    if (cm <= limite_distanc && cm > limite_distanc - 10) // se a distancia em cm for menor que o limite e maior que o limite - 100 não manda sinal ao led verde
+    {
+        digitalWrite(led_verde, HIGH);
+        digitalWrite(led_amarel, LOW);
+        digitalWrite(led_verm, LOW);
+
+        digitalWrite(buzer_pin, HIGH);
+        delay(100);
+      	digitalWrite(buzer_pin, LOW);
+        delay(100);
+    }
+
+    if (cm <= limite_distanc - 10 && cm > limite_distanc - 30 ) // se a distancia em cm for menor que o limite e maior que o limite - 100 não manda sinal ao led verde
+    {
+        digitalWrite(led_verde, LOW);
+        digitalWrite(led_amarel, HIGH);
+        digitalWrite(led_verm, LOW);
+
+        digitalWrite(buzer_pin, HIGH);
+        delay(400);
+      	digitalWrite(buzer_pin, LOW);
+
+    }
+
+    if (cm <= limite_distanc - 30 && cm > limite_distanc - 50 ) // se a distancia em cm for menor que o limite e maior que o limite - 100 não manda sinal ao led verde
+    {
+        digitalWrite(led_verde, LOW);
+        digitalWrite(led_amarel, LOW);
+        digitalWrite(led_verm, HIGH );
+
+        digitalWrite(buzer_pin, HIGH);
+        delay(800);
+      	digitalWrite(buzer_pin, LOW);
+
+    }
+}
